b766cc9586646af881eeda95521e429940473b2b
BELEM ABDOUL RAHIM <32723816+BELEMRAHIM@users.noreply.github.com>
2023-12-10 02:09:27 +0000

    init

---
+++ nanagestion/nanagestion/src/main/java/bf/solinno/nanagestion/NanagestionApplication.java
@@ -0,0 +1,13 @@
+package bf.solinno.nanagestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanagestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanagestionApplication.class, args);
+	}
+
+}
