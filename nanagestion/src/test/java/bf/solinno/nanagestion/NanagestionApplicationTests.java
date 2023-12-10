1dd96d14a65bda2a723bfa4b40fd442f63e7f7a1
HP <belemabdoulrahim@gmail.com>
2023-12-10 02:00:24 +0000

    init

---
+++ nanagestion/src/test/java/bf/solinno/nanagestion/NanagestionApplicationTests.java
@@ -0,0 +1,13 @@
+package bf.solinno.nanagestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanagestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
